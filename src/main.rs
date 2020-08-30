008b29d0fb7e33d62b042a433416bea77fee396f
Derek Barnes <dsbarnes@Dereks-iMac.attlocal.net>
2020-08-30 13:50:42 -0400

    sketching out ideas

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,45 @@ use std::fs;
 use shellexpand::tilde;
 use std::path::Path;
 
+/*
+ * Get the file from it's normal location
+ * Compare that file to the one in the git repo
+ * if different move the contents of the normal location into the git repo
+ * else continue
+ * for each of the files I want to check
+ * 
+ * git commit
+ * push a git branch called dotfile-update-{timestamp}
+ * 
+ */
+const DOTFILES: [(&str, &str); 3] = [
+    ("~/Documents/dotfiles/.config/nvim/init.vim",
+     "~/.config/nvim/init.vim"),
+
+    ("~/Documents/dotfiles/.bash-git-prompt/theme/DSBarnes.bgptheme",
+     "/usr/local/opt/bash-git-prompt/share/themes"),
+
+    ("~/Documents/dotfiles/.bashrc",
+     "~/.bashrc"),
+];
+
+
+fn read_path_to_string(input: String) -> String{
+    let init = shellexpand::tilde(&input);
+    let path = Path::new(init.as_ref());
+    fs::read_to_string(path).unwrap()
+}
+
+
+fn create_changeset(f1: String, f2: String) -> Changeset {
+    Changeset::new(
+        &read_path_to_string(f1), 
+        &read_path_to_string(f2), 
+        "\n"
+    )
+}
+
+
 fn main() {
-    let cow_thing = shellexpand::tilde("~/.config/nvim/init.vim");
-    let cow_path = Path::new(cow_thing.as_ref());
-    let config_nvim = fs::read_to_string(cow_path);
-    println!("{:?}", config_nvim);
-
-    // let changeset = Changeset::new(
-    //     "big buttholes are the worst, or the best?", 
-    //     "big vagface are the best, and the best?", 
-    //     " ");
-
-    // assert_eq!(changeset.diffs, vec![
-    //     Difference::Same("big".to_string()),
-    //     Difference::Rem("buttholes".to_string()),
-    //     Difference::Add("vagface".to_string()),
-    //     Difference::Same("are the".to_string()),
-    //     Difference::Rem("worst, or".to_string()),
-    //     Difference::Add("best, and".to_string()),
-    //     Difference::Same("the best?".to_string()),
-    // ])
+    println!("Happy hacking!");
 }
